f7abeded46e2ae4707ec89779f13421abcefbc5e
Aditya Bhandari <rpsh2823@ramadevipublicschool.com>
2026-06-19 05:20:58 +0000

    Sync LeetCode submission - Find the Highest Altitude (cpp)

---
+++ my-folder/problems/find_the_highest_altitude/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+
+        vector<int> result(n + 1);
+        result[0] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            result[i] = result[i - 1] + gain[i - 1];
+        }
+
+        return *max_element(result.begin(), result.end());
+    }
+};
