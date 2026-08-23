64714904aa2b06f6fbbc9523ae438e7d7e973ca0
Aditya Bhandari <rpsh2823@ramadevipublicschool.com>
2026-08-23 15:38:15 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ my-folder/problems/palindrome_number/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool answer = true;
+        if(x < 0){
+            return false;
+        }
+        else{
+            string str = to_string(x);
+            int n = str.length();
+            for(int i = 0 ; i < n/2 ; i++){
+                if(str[i] != str[n - i - 1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
